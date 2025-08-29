6d60d43b74665e295292220bea8954712ddaa39b
lilixbaranyai <lilibaranyai05@gmail.com>
2025-08-29 19:12:52 +0200

    Form elemek beszúrása

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-const gomb = document.getElementById("gomb-id");
-gomb.addEventListener("click", () => {
-  alert("Ez a meglévő gomb működik!");
-});
+/*Beküldés gomb*/
+const button1 = document.getElementById('ending')
+button1.addEventListener("click", bekuldes)
+function bekuldes(){
+  alert("Beküldve!")
+}
+
+const button2 = document.getElementById('date')
+button2.addEventListener("click", ido)
+function ido(){
+  button2.classList.toggle('selected');
+}
