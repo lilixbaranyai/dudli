8b27f4314ce513c9a77fc81c751494229d2c1d18
lilixbaranyai <lilibaranyai05@gmail.com>
2025-09-11 13:44:46 +0200

    javascript

--- main.js
+++ main.js
@@ -4,9 +4,14 @@ button1.addEventListener("click", bekuldes)
 function bekuldes(){
   alert("Beküldve!")
 }
+// if (placeholder = null){
+//   alert("Nem teljes a kitöltés!")
+// }
+// kellene jelzés, hogyha valami üres, hogy akkor ne a beküldve üzenetet dobja ki
 
 const button2 = document.getElementById('date')
 button2.addEventListener("click", ido)
 function ido(){
   button2.classList.toggle('selected');
 }
+
